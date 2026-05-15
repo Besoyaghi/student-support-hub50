0cee73464e1256d6e64ff7484e6a5657311ea8f9
Besoyaghi <basilmustafa265@yahoo.com>
2026-05-15 16:00:31 +0400

    Connect Supabase config

---
+++ assets/js/supabase-config.js
@@ -0,0 +1,4 @@
+window.SSHUB_SUPABASE = {
+  url: "https://kukokqrikizyrkfsocb.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImprdWtva3FyamtpenZya2Zzb2NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NDQ4NDIsImV4cCI6MjA5NDQyMDg0Mn0.Oc8d-UzUS-nBkfso_VfboNVh9usCIyKvjvzzS7XR2nE"
+};
